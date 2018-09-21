98a702a0e56a229c1574cc3428896cf9efa9a9a6
arshiam2 <arshiam2@illinois.edu>
2018-09-21 17:25:32 -0500

    deployments

---
+++ npm-deployment/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ol>
+            <h3> Walk to latea</h3> 
+            <h3> See Megha at latea</h3> 
+            <h3> Get boba </h3> 
+        </ol>
+      </div>
+    );
+  }
+}
+
+export default App;
